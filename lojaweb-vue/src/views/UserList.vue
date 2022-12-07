cc4913c12694ea8d5cabcda78e906134558a62d9
Fernando11000 <fernandoferreira10@hotmail.com.br>
2022-12-07 20:35:19 -0300

    update model Endereco

--- lojaweb-vue/src/views/UserList.vue
+++ lojaweb-vue/src/views/UserList.vue
@@ -1,5 +1,39 @@
+<template>
+    <NavBar />
+    <section class="container">
+        <h2>Usuarios</h2>
+        <table class="table">
+            <thead>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Data Nasc.</th>
+                <th>CPF</th>
+                <th>Foto Perfil</th>
+                <!-- <th>Senha</th> -->
+                <th>Tel.</th>
+                <th>Ativo</th>
+            </thead>
+            <tbody>
+                <tr v-for="usuario in Usuarios" :key="usuario.id_usuario">
+                    <td>{{ usuario.id_usuario }}</td>
+                    <td>{{ usuario.nome }}</td>
+                    <td>{{ usuario.email }}</td>
+                    <td>{{ usuario.data_nasc }}</td>
+                    <td>{{ usuario.cpf }}</td>
+                    <td>{{ usuario.foto }}</td>
+                    <!-- <td>{{ usuario.senha }}</td> -->
+                    <td>{{ usuario.telefone }}</td>
+                    <td>{{ usuario.ativo }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+</template>
 
 <script>
+import NavBar from '../components/NavBar.vue';
 import userService from '../service/userService';
 export default {
     components: { NavBar },
@@ -20,10 +54,7 @@ export default {
     }
 }
 </script>
-<template>
-    <NavBar />
 
-</template>
 <style>
 
 </style>
